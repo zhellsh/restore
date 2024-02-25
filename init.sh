56a08acdfa9e25c367acd684614d0546e27f95bd
redacted <121198893+zhellsh@users.noreply.github.com>
2024-02-25 13:58:48 -0600

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+echo "This script will setup your new install of *nux!"
+echo "Please enter name of backup with filetype ommited:"
+read back
+apt-prep() {
+	sudo apt update
+	sudo apt upgrade -y
+	sudo apt install lz4 vim curl git python3-full -y
+}
+kernel_upgrade() {
+	wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
+	sudo install ubuntu-mainline-kernel.sh /usr/local/bin/
+	sudo ubuntu-mainline-kernel.sh -i
+}
+dev_suite() {
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+	sudo apt install nasm -y
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	cd ~/
+	mkdir -p dev/rust dev/nim dev/nasm
+}
+restore() {
+	lz4 -d $back.tar.lz4
+	tar xvf $back.tar -C oldhome/
+}
+apt-prep
+kernel_upgrade
+dev_suite
+restore
+reboot
